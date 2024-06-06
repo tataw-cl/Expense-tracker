954aa97f40d03bf9187a2706fe5f4b162f0e1afe
tataw-cl <127490325+tataw-cl@users.noreply.github.com>
2024-06-06 21:13:29 +0100

    update

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import signIn from "./components/auth/register";
 import mainSite from "./components/auth/mainSite";
 import SignUp from "./components/auth/signUp";
+import { redirect } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="./components/auth/register">
-          <SignUp />
-        </Route>{" "}
-        <Route path="./components/auth/mainSite">
-          <mainSite />
-        </Route>{" "}
-      </Switch>{" "}
+      <Route exact path="/register">
+        {" "}
+        {authCheck ? <Redirect to="/mainSite" /> : <SignUp />}{" "}
+      </Route>{" "}
+      <Route exact path="/mainSite">
+        <mainSite />
+      </Route>{" "}
+      <Route exact path="/signIn">
+        <signIn />
+      </Route>{" "}
     </Router>
   );
 }
